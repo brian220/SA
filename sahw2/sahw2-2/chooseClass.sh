7bff305a79f1aea59a3cb9cf7902e50149f03be7
Huang-Chao Yu <hcyu@bsd1.cs.nctu.edu.tw>
2018-10-09 01:51:53 +0800

    finish the first edition of the choose class and time parsing

--- sahw2/sahw2-2/chooseClass.sh
+++ sahw2/sahw2-2/chooseClass.sh
@@ -1,8 +1,48 @@
 #!/bin/sh
 classMenu=""
 i=1
-while read line; do
+while read line;
+do
   classMenu=$classMenu"$i $line off "
   i=$(($i+1))
 done<cos_information.txt
-dialog --buildlist "Course Regestration System" 100 200 40 $classMenu
+chooseNum=`dialog --stdout --buildlist "Course Regestration System" 100 200 40 $classMenu`
+echo $chooseNum | awk -F " " '{for(i=1; i<=NF; i++) print $i > "chooseNum.txt" }'
+rm -f chooseClassName.txt
+rm -f chooseClassTime.txt
+storeChooseClass() {
+  while read line;
+  do
+    awk NR==$line cos_ename.txt >> "chooseClassName.txt"
+    awk NR==$line cos_time.txt >> "chooseClassTime.txt"
+  done < chooseNum.txt
+}
+
+checkClassTime() {
+  while read timeRow;
+  do
+    echo $timeRow |
+        awk '{
+          currentTime="";
+          split ($0, timeArray, "");
+          for (i=1; i < length($0); i++) {
+              if (timeArray[i]~/[0-9]/){
+                if (timeArray[i+1]~/[A-Z]/ && timeArray[i+2]!~/[A-Z]/) {
+                  currentTime = current + TimetimeArray[i] + timeArray[i+1] + ",";
+                }
+                else if (timeArray[i+1]~/[A-Z]/ && timeArray[i+2]~/[A-Z]/ && timeArray[i+3]!~/[A-Z]/ ) {
+                  currentTime = currentTime + timeArray[i] + timeArray[i+1] + "," + timeArray[i] + timeArray[i+2] + ",";
+                }
+                else if (timeArray[i+1]~/[A-Z]/&& timeArray[i+2]~/[A-Z]/ && timeArray[i+3]~/[A-Z]/ && timeArray[i+3]!~/[A-Z]/) {
+                  currentTime = currentTime + timeArray[i] + timeArray[i+1] + "," + timeArray[i] + timeArray[i+2] + "," + timeArray[i] + timeArray[i+3] + ",";
+                }
+              }
+         }
+          print currentTime;
+       }'
+  done < chooseClassTime.txt
+}
+
+storeChooseClass
+checkClassTime
+
